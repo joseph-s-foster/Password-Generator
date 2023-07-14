f3f72f66dcc6c77bf574f7ec5c04ec228569e525
D3C0D1NG <joseph.s.foster@icloud.com>
2023-07-14 18:01:38 -0500

    adding content

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,51 @@
 // Assignment code here
+function generatePassword() {
+  var passwordLength = parseInt(prompt("Password length must be greater than 8 characters and less than 128 characters."))
+  if (passwordLength < 8 || passwordLength > 128 || Number.isNaN(passwordLength)) {
+    alert("Password length does meet criteria.")
+    return null;
+  }
+
+  var hasUppercase = confirm("Include uppercase characters?")
+  var hasLowercase = confirm("Include lowercase characters?")
+  var hasNumber = confirm("Include number characters?")
+  var hasSpecialCharacter = confirm("Includes special characters?")
+
+  if (!hasUppercase && !hasLowercase && !hasNumber && !hasSpecialCharacter ) {
+    alert("Please choose at least one uppercase, lowercase, number, or special character.")
+    return null;
+  }
+
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var number = "0123456789";
+  var specialCharacter = "!@#$%^&*()";
+  var chosenCharacters = "";
+  var password = "";
+
+  if (hasUppercase) {
+    chosenCharacters += uppercase
+  }
+
+  if (hasLowercase) {
+    chosenCharacters += lowercase
+  }
+
+  if (hasNumber) {
+    chosenCharacters += number
+  }
+
+  if (hasSpecialCharacter) {
+    chosenCharacters += specialCharacter
+  }
+
+  for(var i = 0; i < passwordLength; i++) {
+    password += chosenCharacters.charAt(Math.floor (Math.random() * chosenCharacters.length))
+  }
+
+return password;
+
+}
 
 
 // Get references to the #generate element

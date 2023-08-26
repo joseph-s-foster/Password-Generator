b81cf1c9156129cdaa67bc422ec5c145f6a9c82d
D3C0D1NG <joseph.s.foster@icloud.com>
2023-08-26 15:15:05 -0500

    changed buttom color, and polished UX

--- assets/script.js
+++ assets/script.js
@@ -1,60 +1,48 @@
-// Assignment code here
 function generatePassword() {
-  var passwordLength = parseInt(prompt("Select a number between 8 and 128 to set password length."))
-  if (passwordLength < 8 || passwordLength > 128 || Number.isNaN(passwordLength)) {
-    alert("Password length must be between 8 and 128 characters.")
+  const passwordLength = parseInt(prompt("Enter a number between 8 and 128 to set password length."));
+  const isValidLength = passwordLength >= 8 && passwordLength <= 128 && !Number.isNaN(passwordLength);
+
+  if (!isValidLength) {
+    alert("Password length must be between 8 and 128 characters.");
     return null;
   }
 
-  var hasUppercase = confirm("Select OK to include uppercase characters.")
-  var hasLowercase = confirm("Select OK to include lowercase characters.")
-  var hasNumber = confirm("Select OK to include numerical characters.")
-  var hasSpecialCharacter = confirm("Select OK to include special characters.")
+  const hasUppercase = confirm("Select OK to include uppercase characters.");
+  const hasLowercase = confirm("Select OK to include lowercase characters.");
+  const hasNumber = confirm("Select OK to include numerical characters.");
+  const hasSpecialCharacter = confirm("Select OK to include special characters.");
+  
+  const noCharacterTypesSelected = !hasUppercase && !hasLowercase && !hasNumber && !hasSpecialCharacter;
 
-  if (!hasUppercase && !hasLowercase && !hasNumber && !hasSpecialCharacter) {
-    alert("Password must include uppercase, lowercase, numerical, or special characters.")
+  if (noCharacterTypesSelected) {
+    alert("Password must include uppercase, lowercase, numerical, or special characters.");
     return null;
   }
 
-  var array = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789", "!@#$%^&*()"];
-  var chosenCharacters = "";
-  var password = "";
+  const characterSets = [
+    hasUppercase ? "ABCDEFGHIJKLMNOPQRSTUVWXYZ" : '',
+    hasLowercase ? "abcdefghijklmnopqrstuvwxyz" : '',
+    hasNumber ? "0123456789" : '',
+    hasSpecialCharacter ? "!@#$%^&*()" : ''
+  ];
 
-  if (hasUppercase) {
-    chosenCharacters += array[0]
-  }
+  const chosenCharacters = characterSets.join('');
+  let password = '';
 
-  if (hasLowercase) {
-    chosenCharacters += array[1]
-  }
-
-  if (hasNumber) {
-    chosenCharacters += array[2]
-  }
-
-  if (hasSpecialCharacter) {
-    chosenCharacters += array[3]
-  }
-
-  for (var i = 0; i < passwordLength; i++) {
-    password += chosenCharacters.charAt(Math.floor(Math.random() * chosenCharacters.length))
+  for (let i = 0; i < passwordLength; i++) {
+    password += chosenCharacters.charAt(Math.floor(Math.random() * chosenCharacters.length));
   }
 
   return password;
-
 }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
